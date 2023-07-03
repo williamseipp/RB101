ea71c70a78a7519f2f9d8001e2a3942767c16d88
William Seipp <william.seipp@gmail.com>
2023-07-03 09:52:52 -0700

    improve pseudo-code

--- loan_calculator.rb
+++ loan_calculator.rb
@@ -1,120 +1,30 @@
-## PEDAC TEMPLATE
-
-## assignment / problem statement
-
-## problem
-# formula m = p * (j / (1 - (1 + j)**(-n)))
-
-#     INPUTS
-#     loan_amount = p
-#     apr
-#     loan_duration_years
+# psuedocode
 #
-#     OUTPUTS
-#     monthly_interest_rate = j
-#     loan_duration_months = n
-#     monthly_payment = m
+# 1. introduce the program
+# 1a. greet user
+# 1b. ask for name
+# 1c. tell user what the program does
 #
-# make the requirement explicit
-# using calculator as a base, make a program that
-# calculates the monthly payment for a mortgage given 3 inputs;
+# 2. have user fill out the loan terms sheet
+# 2a. ask user for loan amount and provide examples
+# 2b. validate loan amount
+# 2c. ask user for loan duration and provide examples
+# 2d. validate loan duration
+# 2e. ask user for apr and provide examples
+# 2f. validate apr
 #
-#   * loan amount or principal
-#   * apr
-#   * loan duration in years
+# 3. calculate the monthly payment
+# 3a. apply formula with loan terms given
+# 3b. save the monthly payment
 #
-# clarifying questions
+# 4. display the monthly payment of the loan
+# 4a. show the loan terms in a clean way with the monthly payment
 #
-#   * is interest expressed as a whole number '5' or decimal '0.5'?
-#       as a whole or fractional number with a % after it, i.e 5%
+# 5. ask for further input
+# 5a. ask user if they want to calculate another loan
+# 5b. validate user answer
+# 5c. quit the program or repeat step 1
 #
-#   * how do I convert apr into monthly_interest_rate?
-#       1. convert the apr expressed as a percentage into decimal form
-#           by dividing it by 100
-#       2. convert this value into monthly_interest_rate by dividing
-#           it by the number of months; 12
-#
-#   * how is loan duration expressed?
-#       usually as a number of years, i.e 15 or 30
-#
-# identify the rules
-#
-#   * ask for the necessary pieces of information from the user
-#   * validate that information
-#   * convert that information into the correct form
-#    * convert apr into monthly_interest_rate for the formula
-#    * convert loan_duration_years into loan_duration_months for the formula
-#    * the formula asks for loan duration in months; be explicit
-#   * express the monthly_payment back to user
-#
-
-## examples
-
-# validate understanding of the problem
-#
-# formula m = p * (j / (1 - (1 + j)**(-n)))
-#
-#  apr = 6
-#  loan duration in years = 30
-#  loan amount 100000
-#
-#  j = .005
-#  n = 360
-#  p = 100000
-#
-# mental model of the problem (optional)
-#
-#   ASK for info
-#   VALIDATE info
-#   CONVERT info into required values
-#   APPLY formula and solve for m
-#   display m to user
-#
-## data structure: how do we represent the data from input -> output?
-
-# how do we represent the data that we will work with
-# when converting the input to output
-#
-# again, what are the inputs?
-#
-# loan_amount = string > float
-# apr = string > float
-# loan_duration_years = string > float
-#
-# monthly_payment = float > string ( truncated )
-
-## algorithm: what are the steps?
-
-#   >>   ASK for loan terms
-#     prompt user for loan_amount
-#     if its invalid, tell the user its invalid and ask again
-#     if its valid, save as loan_amount
-#
-#     prompt user for loan_duration_years
-#     if its invalid, tell the user its invalid and ask again
-#     if its valid, save as loan_duration_years
-#
-#     prompt user for apr
-#     if its invalid, tell the user its invalid and ask again
-#     if its valid, save as apr
-#
-#   >>  CONVERT info into required values
-#     convert apr into decimal form of monthly_interest_rate
-#     and save as monthly_interest_rate
-#
-#     convert loan_duration_years into loan_duration_months
-#
-#   >>  APPLY formula and solve for m
-#     apply formula with loan_amount, loan_duration_months
-#     and monthly_interest_rate and save the result as monthly_payment
-#
-#   >>  DISPLAY m to user
-#     display monthly_payment in the proper format
-#
-
-## code
-
-#     introduce the program
 def prompt(message)
   puts("\n#{message} \n")
 end
